aac34b01e3ed4ff4c7d05f5873599bf9f088974f
jcummins <jcummins42prague@gmail.com>
2024-06-11 16:47:56 +0200

    "Commit on branch john"

--- src/main.c
+++ src/main.c
@@ -6,8 +6,13 @@
 /*   By: jcummins <jcummins@student.42prague.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:30:48 by jcummins          #+#    #+#             */
-/*   Updated: 2024/06/11 16:31:07 by jcummins         ###   ########.fr       */
+/*   Updated: 2024/06/11 16:47:01 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	main(int argc, char *argv[])
+{
+	return (0);
+}
